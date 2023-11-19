77e1c5698315e8454213933535fddb603a909258
BVBFD <lsevina126@gmail.com>
2023-11-19 15:28:21 +0900

    Explain(Finding-Subset):Added Explain about Finding-Subset with my learning and thoughts

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 function solution(n) {
   let answer = [];
-  let ch = Array.from({ length: n + 1 }, () => 0);
+  const ch = Array.from({ length: n + 1 }, () => 0);
 
-  function DFS(v) {
-    if (v === n + 1) {
+  function DFS(depth) {
+    if (depth === n + 1) {
       let tmp = '';
-      for (let i = 1; i <= n; i++) {
+      for (let i = 1; i <= n + 1; i++) {
         if (ch[i] === 1) tmp += i + ' ';
       }
-
-      if (tmp.length > 0) {
-        answer.push(tmp.trim());
-      }
+      if (tmp.length > 0) answer.push(tmp.trim());
     } else {
-      ch[v] = 1;
-      DFS(v + 1);
-      ch[v] = 0;
-      DFS(v + 1);
+      ch[depth] = 1;
+      // 먼저 첫번쨰 DFS에서 끝까지 가지가 뻗고,
+      DFS(depth + 1);
+      // 가지를 끝까지 뻗었으면 그 전으로 다시 빽해서
+      ch[depth] = 0;
+      // 다른 쪽으로 또 가지를 뻗는다.
+      DFS(depth + 1);
     }
   }
 
   DFS(1);
-
   return answer;
 }
 
