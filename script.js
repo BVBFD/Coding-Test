13532b61f759d659679ab6014360bfdf0afa94e0
BVBFD <lsevina126@gmail.com>
2023-12-02 17:48:12 +0900

    Study(8-13):Understand the logic of the code

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-function solution(n) {
-  let answer = [];
-  const ch = Array.from({ length: n + 1 }, () => 0);
-
-  function DFS(depth) {
-    if (depth === n + 1) {
-      let tmp = '';
-      for (let i = 1; i <= n + 1; i++) {
-        if (ch[i] === 1) tmp += i + ' ';
-      }
-      if (tmp.length > 0) answer.push(tmp.trim());
+function solution(n, f) {
+  let answer, flag;
+  let dy = Array.from(Array(11), () => Array(11).fill(0));
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let p = Array.from({ length: n }, () => 0);
+  let b = Array.from({ length: n }, () => 0);
+  function combi(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+  }
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L === n && sum === f) {
+      answer = p.slice();
+      flag = 1;
     } else {
-      ch[depth] = 1;
-      // 먼저 첫번쨰 DFS에서 끝까지 가지가 뻗고,
-      DFS(depth + 1);
-      // 가지를 끝까지 뻗었으면 그 전으로 다시 빽해서
-      ch[depth] = 0;
-      // 다른 쪽으로 또 가지를 뻗는다.
-      DFS(depth + 1);
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          p[L] = i;
+          DFS(L + 1, sum + b[L] * p[L]);
+          ch[i] = 0;
+        }
+      }
     }
   }
 
-  DFS(1);
+  for (let i = 0; i < n; i++) {
+    b[i] = combi(n - 1, i);
+  }
+  DFS(0, 0);
   return answer;
 }
 
-console.log(solution(3));
+console.log(solution(4, 16));
