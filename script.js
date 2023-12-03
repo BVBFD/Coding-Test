2524aebdc611353b5c4df1336ec6db13c4ba9066
lsevina126 <lsevina126@gmail.com>
2023-12-03 20:26:12 +0900

    Study(8-13):Study about the question 8-13

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 function solution(n, f) {
   let answer, flag;
-  let dy = Array.from(Array(11), () => Array(11).fill(0));
-  let ch = Array.from({ length: n + 1 }, () => 0);
+
   let p = Array.from({ length: n }, () => 0);
   let b = Array.from({ length: n }, () => 0);
+
+  // 조합수 구하기
+  let dy = Array.from(Array(11), () => Array(11).fill(0));
   function combi(n, r) {
-    if (dy[n][r] > 0) return dy[n][r];
+    if (dy[n][r]) return dy[n][r];
     if (n === r || r === 0) return 1;
     else return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
   }
+  // 조합수 구하기
+
+  // 순열구하기
+  // 중복 순열은 여기서 ch(체크배열)만 빼면 됨.
+  let ch = Array.from({ length: n + 1 }, () => 0);
   function DFS(L, sum) {
     if (flag) return;
     if (L === n && sum === f) {
@@ -25,6 +32,8 @@ function solution(n, f) {
       }
     }
   }
+  // 중복 순열은 여기서 ch(체크배열)만 빼면 됨.
+  // 순열구하기
 
   for (let i = 0; i < n; i++) {
     b[i] = combi(n - 1, i);
