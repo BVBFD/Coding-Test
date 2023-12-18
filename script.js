ffe533c605161a91546c9e3c09889bf447051eee
BVBFD <lsevina126@gmail.com>
2023-12-18 20:25:59 +0900

    Solved(9-2):경로탐색 문제 풀이 및 이해하기

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
-function solution(n, k, arr, m) {
+function solution(n, arr) {
   let answer = 0;
-  let tmp = Array.from({ length: k });
+  let graph = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  let path = [];
 
-  function DFS(depth, s, sum) {
-    if (depth === k) {
-      if (sum % m === 0) {
-        answer++;
-        console.log(tmp, sum, answer);
-      }
+  // 갈수 있는 방향 그래프 그리기
+  for (let [a, b] of arr) {
+    graph[a][b] = 1;
+  }
+
+  // console.log(graph);
+
+  // v라는 정점에서 i로 갈수 있느냐?
+  function DFS(v) {
+    if (v === n) {
+      answer++;
+      // console.log(path);
     } else {
-      for (let i = s; i < n; i++) {
-        tmp[depth] = arr[i];
-        DFS(depth + 1, i + 1, sum + arr[i]);
+      for (let i = 1; i <= n; i++) {
+        if (graph[v][i] === 1 && ch[i] === 0) {
+          ch[i] = 1;
+          path.push(i);
+          DFS(i);
+          ch[i] = 0;
+          // 트리에서 그 전 트리로 다시 올라갈땐 다시 pop해줘서 빼줘야함
+          path.pop();
+        }
       }
     }
   }
 
-  DFS(0, 0, 0);
+  // 1은 출발점이니깐 무조건 들어가야함
+  path.push(1);
+  // 처음에 8가지가 나오는데 ch[1] 체크를 안해줘서 그런거임
+  ch[1] = 1;
+  DFS(1);
   return answer;
 }
 
-let arr = [2, 4, 5, 8, 12];
-console.log(solution(5, 3, arr, 6));
+let arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(solution(5, arr));
