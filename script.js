7109ca681c47110cd174eeba834b15675aaf7891
BVBFD <lsevina126@gmail.com>
2024-01-11 19:37:23 +0900

    Question 9-4 solved

--- script.js
+++ script.js
@@ -1,52 +1,36 @@
-function solution(n, arr) {
+function solution(board) {
   let answer = 0;
-  let graph = Array.from(Array(n + 1), () => Array(n + 1).fill(0));
-  let ch = Array.from({ length: n + 1 }, () => 0);
-  let path = [];
-
-  // 갈수 있는 방향 그래프 그리기
-  for (let [a, b] of arr) {
-    graph[a][b] = 1;
-  }
-
-  // console.log(graph);
-
-  // v라는 정점에서 i로 갈수 있느냐?
-  function DFS(v) {
-    if (v === n) {
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+  function DFS(x, y) {
+    if (x === 6 && y === 6) {
       answer++;
-      // console.log(path);
     } else {
-      for (let i = 1; i <= n; i++) {
-        if (graph[v][i] === 1 && ch[i] === 0) {
-          ch[i] = 1;
-          path.push(i);
-          DFS(i);
-          ch[i] = 0;
-          // 트리에서 그 전 트리로 다시 올라갈땐 다시 pop해줘서 빼줘야함
-          path.pop();
+      for (let k = 0; k < 4; k++) {
+        let nx = x + dx[k];
+        let ny = y + dy[k];
+        if (nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6 && board[nx][ny] === 0) {
+          board[nx][ny] = 1;
+          DFS(nx, ny);
+          board[nx][ny] = 0;
         }
       }
     }
   }
 
-  // 1은 출발점이니깐 무조건 들어가야함
-  path.push(1);
-  // 처음에 8가지가 나오는데 ch[1] 체크를 안해줘서 그런거임
-  ch[1] = 1;
-  DFS(1);
+  board[0][0] = 1;
+  DFS(0, 0);
   return answer;
 }
 
 let arr = [
-  [1, 2],
-  [1, 3],
-  [1, 4],
-  [2, 1],
-  [2, 3],
-  [2, 5],
-  [3, 4],
-  [4, 2],
-  [4, 5],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 0, 0, 0, 0, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0],
 ];
-console.log(solution(5, arr));
+
+console.log(solution(arr));
